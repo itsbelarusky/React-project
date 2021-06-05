a5ca4a6b15d764a4c1f3c51ce189013ecc00b9e8
admin <explosionartx@gmail.com>
2021-06-06 01:17:11 +0300

    add react

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/25/Intel_logo_%282006-2020%29.jpg" alt=""/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img
+                src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"
+                alt=""/>
+            <div>
+                ava+descript
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+
+                New Post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
