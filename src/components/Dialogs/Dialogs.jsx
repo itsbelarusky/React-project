33493a9bc13a63299f2cf37e7e659307f83e7918
admin <explosionartx@gmail.com>
2021-06-14 23:32:35 +0300

    add clean style

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialogsItem}>
-                    Evgeniy
+                <div className={s.dialogsItem + ' ' + s.active}>
+                    <NavLink to='/dialogs/2'>Evgeniy</NavLink>
                 </div>
                 <div className={s.dialogsItem}>
-                    Diana
+                    <NavLink to='/dialogs/2'>Diana</NavLink>
                 </div>
                 <div className={s.dialogsItem}>
-                    Alina
+                    <NavLink to='/dialogs/3'>Maksim</NavLink>
                 </div>
                 <div className={s.dialogsItem}>
-                    Maksim
+                    <NavLink to='/dialogs/4'>Oleg</NavLink>
                 </div>
                 <div className={s.dialogsItem}>
-                    Oleg
+                    <NavLink to='/dialogs/5'>Vlad</NavLink>
                 </div>
                 <div className={s.dialogsItem}>
-                    Vlad
+                    <NavLink to='/dialogs/6'>Alina</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
