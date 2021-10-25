c185d24747739de6e34563664d2220ea66cf60cf
itsbelarusky <explosionartx@gmail.com>
2021-10-26 00:17:41 +0300

    refactor login

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -15,7 +15,7 @@ const authReducer = (state = initialState, action) => {
             return {
                 ...state,
                 ...action.payload,
-                isAuth: true
+
             };
         default:
             return state;
